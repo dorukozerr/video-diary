18e9fde12c6a6477f6d0320b2c518ccf2381d52b
Doruk Özer <dorukozer@protonmail.com>
2024-12-13 22:42:19 +0300

    feat: eslint initialized

---
+++ .eslintrc.ts
@@ -0,0 +1,10 @@
+import {} from "eslint-config-expo"
+import {} from "eslint"
+
+// https://docs.expo.dev/guides/using-eslint/
+const eslint = {
+  extends: 'expo',
+  ignorePatterns: ['/dist/*'],
+};
+
+export default eslint
