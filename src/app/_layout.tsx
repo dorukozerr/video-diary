985bc649e85687ee4137d3c020f129486dcf9894
Doruk Özer <dorukozer@protonmail.com>
2024-12-17 15:56:07 +0300

    fix: safe area view background color

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,7 +2,7 @@ import { Slot } from 'expo-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useThemeStore } from '@/stores/theme-store';
 import { themes } from '@/utils/constants';
-import { SafeAreaView, View } from '@/components/ui/themed-primitives';
+import { SafeAreaView } from '@/components/ui/themed-primitives';
 import { Header } from '@/components/layout/header';
 import { Navbar } from '@/components/layout/navbar';
 
@@ -13,17 +13,11 @@ const queryClient = new QueryClient();
 const Layout = () => {
   const activeTheme = useThemeStore((state) => state.activeTheme);
 
-  // TODO: When safe area background becomes white ios simulator topbar becomes
-  // unvisible. Like time, battery, wifi status ..etc. Is it happening because I
-  // implemented theming wrong or something else idk. I set SafeAreaView background
-  // to black to solve this but if users system theme is dark this can make topbar
-  // invisible again. Must look into this further.
-
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaView
         style={themes[activeTheme]}
-        className='flex h-full w-full flex-col items-start justify-start bg-black'
+        className='flex h-full w-full flex-col items-start justify-start bg-background'
       >
         <Header />
         <Navbar />
